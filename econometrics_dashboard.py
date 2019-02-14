e8a6c585b40b08032c4fc76d2b6aee448c51f2a0
Simonbed <37986393+Simonbed@users.noreply.github.com>
2019-02-13 19:26:29 -0500

    Update econometrics_dashboard.py

--- econometrics_dashboard.py
+++ econometrics_dashboard.py
@@ -1,1 +1,36 @@
 
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+import pandas as pd
+import sqlite
+import pandas.io.sql as pdsql
+
+from dash.dependencies import Input, Output
+
+
+conn = sqlite.connect("exemple.db")
+sql = """ 
+SELECT top 5 *
+FROM Table
+"""
+
+df = pdsql.read_sql(sql, conn)
+
+print(df.head())
+
+app = dash.Dash()
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
